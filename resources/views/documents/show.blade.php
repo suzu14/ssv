cf62cb51cf6ac98680ac3915652c9b21a7c272b7
suzu14 <suzu.u.366@gmail.com>
2023-06-29 13:58:23 +0900

    css(home)

--- resources/views/documents/show.blade.php
+++ resources/views/documents/show.blade.php
@@ -8,67 +8,108 @@
     </head>
     <body>
         <header>
-            <h1>Shift Superview</h1>
-            <nav>
-                <ul>
-                    <li><a href='/'>ホーム</a></li>
-                    <li><a href='/create'>新規報告</a></li>
-                    <li><a href='/documents/index'>書類提出</a></li>
-                </ul>
-            </nav>
-            <div>ログイン中：<a href='/profile'>{{ Auth::user()->name }}</a></div>
+            <section class="header content">
+                <div class="header main">
+                    <h1 class="app-name">Shift Superview</h1>
+                    <nav>
+                        <ul>
+                            <li><a href='/'>ホーム</a></li>
+                            <li><a href='/create'>新規報告</a></li>
+                            <li><a href='/documents/index'>書類提出</a></li>
+                            @if (Auth::user()->roll != 2)
+                            <li><a href='/documents/create'>新規書類作成</a></li>
+                            <li><a href='/groups/create'>新規グループ作成</a></li>
+                            @endif
+                        </ul>
+                    </nav>
+                </div>
+                <div class="header menu">
+                    <p>ログイン中：<a href='/profile'>{{ Auth::user()->name }}</a></p>
+                    <p><a href='/users/{{ Auth::user()->id }}'>ユーザープロフィール</a></p>
+                    <p><form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <x-dropdown-link :href="route('logout')"
+                                onclick="event.preventDefault();
+                                            this.closest('form').submit();">
+                            {{ __('ログアウト') }}
+                        </x-dropdown-link>
+                    </form></p>
+                </div>
+            </section>
         </header>
-        
-        <div class='pagetitle'>
-            <h2>{{ $document->name }}</h2>
-        </div>
-        
-        @if ($document->path == NULL)
-        <p>ファイルが未提出です</p>
-        @else
-        <p>以下のファイルが提出済みです</p>
-        <div>
-            <iframe src="{{ $document->path }}" width="100%" height="500vh"></iframe>
-        </div>
-        @endif
-        
-        @section('preview')
-        @if ($document->path)
-            <img src="{{ $document->path }}">
-        @endif
-        @endsection
-        
-        <form action="/documents/{{ $document->id }}" method="POST" enctype="multipart/form-data">
-            @csrf
-            @method('PUT')
-            <input type="file" name="document_file">
-            @if ($document->path != NULL)
-                <input type="text" name="document[path]" value="{{ $document->path }}" readonly
-            @endif
-            <input type="text" name="document[status]" value=2>
-            <input type="submit" value="アップロード">
-        </form>
-        
-        @canany ('admin', 'leader')
-            <form action="/documents/{{ $document->id }}" method="POST">
-                @csrf
-                @method('put')
-                    <div class='approval'>
+        <div class="main-aside">
+            <main>
+                <h2 class='pagetitle'>{{ $document->name }}</h2>
+                <section class="submit-status">
+                    @if ($document->path == NULL)
+                    <p>ファイルが未提出です</p>
+                    @else
+                    <p>以下のファイルが提出済みです</p>
+                    <div>
+                        <iframe src="{{ $document->path }}" width="100%" height="500vh"></iframe>
+                    </div>
+                    @endif
+                    
+                    @section('preview')
+                    @if ($document->path)
+                        <img src="{{ $document->path }}">
+                    @endif
+                    @endsection
+                </section>
+                <section class="submit-form">
+                    <form action="/documents/{{ $document->id }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
+                        <input type="file" name="document_file">
+                        @if ($document->path != NULL)
+                            <input type="text" name="document[path]" value="{{ $document->path }}" readonly
+                        @endif
+                        <input type="text" name="document[status]" value=2>
+                        <input type="submit" value="アップロード">
+                    </form>
+                </section>
+                @canany ('admin', 'leader')
+                <section class="approval-form">
+                    <form action="/documents/{{ $document->id }}" method="POST">
+                        @csrf
+                        @method('put')
+                        <div class='approval'>
                         @if ($document->status == 2)
                             <button type="submit" name="document[status]" value=3 onclick="submitApproval({{ $document->id }})">承認</button>
                         @elseif ($document->status == 3)
                             <button type="button" disabled>承認済み</button>
                         @else
                         @endif
-                    </div>
-            </form>
-        
-            <form action="/documents/{{ $document->id }}" id="form_{{ $document->id }}" method="post">
-                @csrf
-                @method('DELETE')
-                <button type="button" onclick="deleteDocument({{ $document->id }})">削除</button> 
-            </form>
-        @endcan
+                        </div>
+                    </form>
+                </section>
+                <section class="delete-form">
+                <form action="/documents/{{ $document->id }}" id="form_{{ $document->id }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button type="button" onclick="deleteDocument({{ $document->id }})">削除</button> 
+                </form>
+                </section>
+                @endcan
+            </main>
+            <aside class='groups'>
+                <nav>
+                    <h3>参加中のグループ</h3>
+                    @foreach (Auth::user()->groups as $group)
+                    <ul>
+                        <li><a href="/groups/{{ $group->id }}">{{ $group->name }}</a></li>
+                    </ul>
+                    @endforeach
+                </nav>
+            </aside>
+        </div>
+        <footer>
+            <section class="footer content">
+                <small>
+                    <p>フッター</p>
+                </small>
+            </section>
+        </footer>
         
         <script>
             function submitApproval(id) {
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>活動履歴一覧</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -10,25 +11,38 @@
     </head>
     <body>
         <header>
-            <h1>Shift Superview</h1>
-            <nav>
-                <ul>
-                    <li><a href='/'>ホーム</a></li>
-                    <li><a href='/create'>新規報告</a></li>
-                    <li><a href='/documents/index'>書類提出</a></li>
-                    @if (Auth::user()->roll == 1)
-                    <li><a href='/groups/create'>新規グループ作成</a></li>
-                    @endif
-                </ul>
-            </nav>
-                <p>ログイン中：<a href='/profile'>{{ $user->name }}</a></p>
-                <p><a href='/users/{{ $user->id }}'>プロフィール</a></p>
+            <section class="header content">
+                <div class="header main">
+                    <h1 class="app-name">Shift Superview</h1>
+                    <nav>
+                        <ul>
+                            <li><a href='/'>ホーム</a></li>
+                            <li><a href='/create'>新規報告</a></li>
+                            <li><a href='/documents/index'>書類提出</a></li>
+                            @if (Auth::user()->roll != 2)
+                            <li><a href='/documents/create'>新規書類作成</a></li>
+                            <li><a href='/groups/create'>新規グループ作成</a></li>
+                            @endif
+                        </ul>
+                    </nav>
+                </div>
+                <div class="header menu">
+                    <p>ログイン中：<a href='/profile'>{{ Auth::user()->name }}</a></p>
+                    <p><a href='/users/{{ $user->id }}'>ユーザープロフィール</a></p>
+                    <p><form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <x-dropdown-link :href="route('logout')"
+                                onclick="event.preventDefault();
+                                            this.closest('form').submit();">
+                            {{ __('ログアウト') }}
+                        </x-dropdown-link>
+                    </form></p>
+                </div>
+            </section>
         </header>
-        
-        <div>
+        <div class="main-aside">
             <main>
                 <h2 class='pagetitle'>活動履歴一覧</h2>
-            
                 <table class='activities'>
                     <thead>
                         <tr>
@@ -60,18 +74,23 @@
                     </tbody>
                 </table>
             </main>
-        
-        
-            @canany(['general', 'admin'])
-                <aside class='groups'>
+            <aside class='groups'>
+                <nav>
                     <h3>参加中のグループ</h3>
                     @foreach ($user->groups as $group)
                     <ul>
                         <li><a href="/groups/{{ $group->id }}">{{ $group->name }}</a></li>
                     </ul>
                     @endforeach
-                </aside>
-            @endcan
+                </nav>
+            </aside>
         </div>
+        <footer>
+            <section class="footer content">
+                <small>
+                    <p>フッター</p>
+                </small>
+            </section>
+        </footer>
     </body>
 </html>
